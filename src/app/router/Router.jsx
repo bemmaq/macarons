e16b2614c871bead1bdce93cad4cd9bfbfd24976
Begimai <bahadurybabegimai@gmail.com>
2024-07-16 11:51:33 +0600

    macarons

---
+++ src/app/router/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../../widgets/Layout/Layout";
+import ErrorPage from "../../widgets/ErrorPage/ErrorPage";
+import HomePage from "../../features/HomePage/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: '',
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children:[
+            {
+                path:'/',
+                element:<HomePage/>
+            }
+        ]
+    },
+]);
+
+export default router;
