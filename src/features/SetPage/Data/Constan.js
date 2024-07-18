6e660182b72fe5a2de009750956bd164d250ad53
Begimai <bahadurybabegimai@gmail.com>
2024-07-18 18:34:57 +0600

    setPage

---
+++ src/features/SetPage/Data/Constan.js
@@ -0,0 +1,22 @@
+import color1 from "../../../shared/assets/img/Group 1.png";
+import color2 from "../../../shared/assets/img/Group 2.png";
+import color3 from "../../../shared/assets/img/Group 3.png";
+import color4 from "../../../shared/assets/img/Group 4.png";
+import color5 from "../../../shared/assets/img/Group 5.png";
+import color6 from "../../../shared/assets/img/Group 6.png";
+
+import svg1 from "../../../shared/assets/svg/color1.svg";
+import svg2 from "../../../shared/assets/svg/color2.svg";
+import svg3 from "../../../shared/assets/svg/color3.svg";
+import svg4 from "../../../shared/assets/svg/color4.svg";
+import svg5 from "../../../shared/assets/svg/color5.svg";
+import svg6 from "../../../shared/assets/svg/color6.svg";
+ 
+export const cardData = [
+    { color: color1, svg: svg1, title: "Готовые наборы →", description: ["Готовые наборы со скидкой.", "Вы можете подобрать набор на подходящий случай."] },
+    { color: color2, svg: svg2, title: "Собрать свой набор →", description: ["Выбрать количество макарун, и выбрать вкусы"] },
+    { color: color3, svg: svg3, title: "Набор с индивидуальной печатью →", description: ["Собрать набор макарон с уникальным дизайном."] },
+    { color: color4, svg: svg4, title: "Свадебные предложения →", description: ["Нежные пирожные макаронс с разными вкусами для", "украшения вашего свадебного торжества"] },
+    { color: color5, svg: svg5, title: "Корпоративные подарки →", description: ["От 85 руб за шт. С уникальным дизайном.", "Приятный комплимент для коллег и партнеров"] },
+    { color: color6, svg: svg6, title: "Оптовые поставки →", description: ["Предложение для кофеен, кафе, отелей и т.д.", "Посмотрите условия сотрудничества и отзывы."] },
+  ];
