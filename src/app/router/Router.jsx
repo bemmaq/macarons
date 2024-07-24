ebc9be75c539beab756480a30220d421eeb92b59
Daiyr <bejsebaevagulmira774@gmail.com>
2024-07-24 23:17:14 +0600

    Contact

--- src/app/router/Router.jsx
+++ src/app/router/Router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../../widgets/Layout/Layout";
 import ErrorPage from "../../widgets/ErrorPage/ErrorPage";
 import HomePage from "../../features/HomePage/HomePage";
-
+import Contact from "../../widgets/Contact/Contact"
 const router = createBrowserRouter([
     {
         path: '',
@@ -12,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<HomePage/>
+            },
+            {
+                path:'/Contact',
+                element:<Contact/>
             }
         ]
     },
--- src/widgets/Contact/Contact.jsx
+++ src/widgets/Contact/Contact.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
-import Container from "../../../../shared/Container/Container";
+import Container from "../../shared/Container/Container";
+import MacaronShopGeoLogo from "./png/MacaronShopGeoLogo.png"
 function Contact() {
   return (
-    <>
-    <h5>Главная  Контакты</h5>
     <Container>
-      <img src="" alt="" />
+      <h2 className='ml-[470px] mt-[70px]'>Главная > Контакты</h2>
+      <div className='flex justify-between items-center w-[900px] ml-[100px]'>
+      <img src={MacaronShopGeoLogo} alt="" />
       <div>
-        <h1>Контакты</h1>
+        <h1 className="text-gray-700 font-Montserrat text-2xl font-semibold leading-51 tracking-tight text-left"
+        >Контакты</h1> <br />
         <div>
-            <h2>Производство</h2>
-            <p>Маршала Тухачевского, 22 <br />Время работы: с 8 до 19:30.
-            </p>
+            <h2 className="text-gray-700 font-Montserrat text-base font-semibold leading-22 tracking-normal text-left"
+            >Производство</h2>
+            <p className="text-gray-700 font-Proxima-Nova text-base font-normal leading-relaxed tracking-normal text-left"
+            >
+            Маршала Тухачевского, 22 <br />Время работы: с 8 до 19:30.
+            </p> <br />
         </div>
         <div>
-            <h2>Пункты самовывоза:</h2>
-            <p>
-            Кафе “Морошка”. Маршала Тухачевского, 22 <br /> (с 8 до 19:30)
-            Кафе “Мята”. Наб канала Грибоедова, 37 <br /> (с 10 до 22)
-            </p>
+            <h2 className="text-gray-700 font-Montserrat text-base font-semibold leading-22 tracking-normal text-left">Пункты самовывоза:</h2>
+            <p className="text-gray-700 font-Proxima-Nova text-base font-normal leading-relaxed tracking-normal text-left">Кафе “Морошка”. Маршала Тухачевского, 22 <br />(с 8 до 19:30) <br />
+            Кафе “Мята”. Наб канала Грибоедова, 37 <br />(с 10 до 22)</p> <br />
         </div>
         <div>
-            <h2>Телефоны:</h2>
-            <p>
-                8 (812) 309-82-88 основной номер <br />
-                8 (981) 841-85-25 для жалоб и предложений
-            </p>
+            <h2 className="text-gray-700 font-Montserrat text-base font-semibold leading-22 tracking-normal text-left">Телефоны:</h2>
+            <p className="text-gray-700 font-Proxima-Nova text-base font-normal leading-relaxed tracking-normal text-left">8 (812) 309-82-88 основной номер <br />
+            8 (981) 841-85-25 для жалоб и предложений</p>
         </div>
       </div>
+      </div>
     </Container>
-    </>
   )
 }
 
