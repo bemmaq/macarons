d2fb006107a919598361a8f2d3575eb0872ca08d
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2024-07-31 13:29:26 +0600

    update

--- src/features/GiftSets/UI/GiftSets.jsx
+++ src/features/GiftSets/UI/GiftSets.jsx
@@ -1,22 +1,15 @@
-
-
 import React from "react";
 import { motion } from "framer-motion";
 import Container from "../../../shared/Container/Container";
 import { giftsetsdata } from "../data/Contants";
 import bag from "../../../shared/assets/svg/bag.svg";
+import { Link } from "react-router-dom";
 
 const giftsets = () => {
   const cardVariants = {
     hidden: { opacity: 0, y: -80 },
     visible: { opacity: 1, y: 0 },
   };
-
-  const buttonVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 },
-  };
-
   const headengVariants = {
     hidden: { opacity: 0, y: -50 },
     visible: { opacity: 1, y: 0 },
@@ -25,7 +18,15 @@ const giftsets = () => {
   return (
     <Container className="relative">
       <p className="text-[#989898] font-proxima text-sm font-normal leading-[17.05px] text-center mt-[80px] mb-[-50px]">
-        Главная >  <span className="text-[#4e9dd3] font-proxima text-sm font-normal leading-[17.05px] text-left">Подарочные наборы</span>
+        <Link
+          to="/"
+          className="text-[#989898] font-proxima text-sm font-normal leading-[17.05px]"
+        >
+          Главная>
+        </Link>
+        <span className="text-[#4e9dd3] font-proxima text-sm font-normal leading-[17.05px] text-left">
+          Подарочные наборы
+        </span>
       </p>
 
       <motion.div
@@ -36,20 +37,22 @@ const giftsets = () => {
           visible: { transition: { staggerChildren: 0.5 } },
         }}
       >
-        {["Свадьба", "8 марта", "Новый год",'Без печати'].map((tittle, index) => (
-          <motion.h1
-            key={index}
-            className="w-[100px] h-[40px]  text-[#000] rounded-[100px]  bg-[#F7EBE5] mb-2 text-center p-2.5 font-proxima text-[14px]"
-            variants={headengVariants}
-            transition={{ duration: 0.5 }}
-          >
-            {tittle}
-          </motion.h1>
-        ))}
+        {["Свадьба", "8 марта", "Новый год", "Без печати"].map(
+          (tittle, index) => (
+            <motion.h1
+              key={index}
+              className="w-[100px] h-[40px]  text-[#000] rounded-[100px]  bg-[#F7EBE5] mb-2 text-center p-2.5 font-proxima text-[14px]"
+              variants={headengVariants}
+              transition={{ duration: 0.5 }}
+            >
+              {tittle}
+            </motion.h1>
+          )
+        )}
       </motion.div>
 
       <h1 className="my-[100px] text-center font-montserrat text-[30px] font-semibold leading-[36.57px]">
-      Подарочные наборы
+        Подарочные наборы
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-[100px]">
@@ -88,34 +91,8 @@ const giftsets = () => {
           </motion.div>
         ))}
       </div>
-
-      <motion.button
-        className="
-          mt-[-40px]
-          mb-[40px]
-          transform -translate-x-1/2
-          w-[293px] 
-          h-[50px] 
-          border 
-          border-[#4E9DD3]
-          rounded-tl-3px 
-          font-montserrat 
-          text-[14px] 
-          font-semibold 
-          leading-[17.07px] 
-          text-center
-          mx-[460px]
-        "
-        initial="hidden"
-        animate="visible"
-        variants={buttonVariants}
-        transition={{ duration: 0.5, delay: giftsetsdata.length * 0.5 }}
-      >
-        Показать ещё
-      </motion.button>
     </Container>
   );
 };
 
 export default giftsets;
-
--- src/widgets/Layout/Layout.jsx
+++ src/widgets/Layout/Layout.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Header from '../Header/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer.jsx/Footer'
-import ProductSet from '../../features/ProductSet/ui/ProductSet'
 
 const Layout = () => {
   return (
     <div>
         <Header/>
         <Outlet/>
-        {/* <ProductSet/> */}
         <Footer/>
     </div>
   )
